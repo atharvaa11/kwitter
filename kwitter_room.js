6e1cf8fb0a700166208657a5ae43190ef54474a4
atharvaa11 <72305083+atharvaa11@users.noreply.github.com>
2021-06-01 20:38:21 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -44,4 +44,17 @@ localStorage.setItem("room_name", name);
 window.location = "Kwitter_page.html";
 
 
+
 }
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+window.location = "index.html";
+
+
+}
+
+
+
+
